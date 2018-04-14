389ea90cebef5fb47add819ad92a3c6f0ff28c15
Andrea Giavatto <emaildiripiego@gmail.com>
2018-04-14 18:17:50 +0100

    Initial Commit

---
+++ Workshop-CollectionView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Workshop-CollectionView
+//
+//  Created by Andrea on 14/04/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
